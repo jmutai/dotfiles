f5e4f7f614590a79499faa173cfc16684c085b08
Mutai Josphat <geekstuff15@gmail.com>
2016-12-29 09:43:14 +0300

    updated

--- cheat-sheets/ispconfig/letsencrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-URL="$1"
-SITE_DIR="/var/www/${1}/ssl"
-
-ln -sf /var/letsencrypt/live/${1}/cert.pem  ${SITE_DIR}/${1}.crt
-ln -sf /var/letsencrypt/live/${1}/privkey.pem ${SITE_DIR}/${1}.key
-ln -sf /var/letsencrypt/live/${1}/chain.pem ${SITE_DIR}/${1}.bundle
