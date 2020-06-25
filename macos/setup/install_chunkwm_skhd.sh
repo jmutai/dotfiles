e1c4af3023729929f529fc6036882dd996929d96
Josphat Mutai <jkmutai@MacBook-Pro.local>
2020-06-26 02:34:53 +0300

    updated

--- macos/setup/install_chunkwm_skhd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-brew tap crisidev/homebrew-chunkwm
-brew install --HEAD --with-tmp-logging chunkwm
-brew install --HEAD --with-logging  koekeishiya/formulae/skhd
-
-# Enable services
-
- brew services start chunkwm skbd
